da23687cb90eaf57ab634d4828fdce614a664e0f
lxc <lxc23@drexel.edu>
2024-05-08 22:18:05 -0400

    updated everything

--- grammar.js
+++ grammar.js
@@ -1,37 +1,152 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
+const operators = [
+  "-", "-=", "!=", "*", "**", "**=",
+  "*=", "/", "//", "//=", "/=", "&",
+  "&=", "%", "%=", "^", "^=", "+",
+  "->", "+=", "<", "<<", "<<=", "<=",
+  "<>", "=", ":=", "==", ">", ">=",
+  ">>", ">>=", "|", "|=", "~", "@=",
+  "and", "in", "is", "not", "or",
+]
+
 module.exports = grammar({
   name: "jinja2",
 
+  word: $ => $.identifier,
+
   rules: {
-    // Main entry point
     template: $ => repeat(choice(
-      $.statement,
-      $.expression,
-      $.comment,
+      $.jinja_statement,
+      $.jinja_expression,
+      $.jinja_comment,
       $._text,
     )),
 
     // Jinja2 statements
-    statement: $ => seq(
-      '{%',
-      new RegExp('([^%]|%[^}])*%}'),
+    jinja_statement: $ => seq(
+      /\{%-?/,
+      $._expression,
+      /-?%\}/,
     ),
 
     // Jinja2 expressions
-    expression: $ => seq(
+    jinja_expression: $ => seq(
       '{{',
-      new RegExp('[^}}]*}}'),
+      $._expression,
+      '}}',
     ),
 
     // Jinja2 comments
-    comment: $ => seq(
+    jinja_comment: $ => seq(
       '{#',
-      new RegExp('([^#]|#[^}])*#+}'),
+      repeat(/./),
+      '#}',
     ),
 
-    // Everything else
-    _text: $ => new RegExp('.')
+    // Content
+    _expression: $ => choice(
+      $.keyword,
+      $.identifier,
+      $.fn_declaration,
+      $._primitive,
+      $.wrappers,
+      $._dot_index,
+      $.unary_expression,
+      $.binary_expression,
+    ),
+
+    _primitive: $ => choice(
+      $.string,
+      $.integer,
+      $.float,
+      $.boolean,
+      $.none,
+    ),
+
+    _dot_index: $ => prec(4, seq(
+      $.identifier,
+      optional(repeat1(seq('.', $.identifier)))
+    )),
+
+    fn_declaration: $ => prec.left(3, seq(
+      field('name', $.identifier),
+      '(',
+      field('arguments', optional($._parameters)),
+      ')'
+    )),
+
+    _parameters: $ => repeat1(seq($._expression, optional(','))),
+
+    wrappers: $ => choice(
+      $._parenthesis,
+      $._bracket,
+      $._braces,
+    ),
+
+    _parenthesis: $ => seq(
+      '(',
+      optional($._expression),
+      ')',
+    ),
+
+    _bracket: $ => seq(
+      '[',
+      optional($._expression),
+      ']',
+    ),
+
+    _braces: $ => seq(
+      '{',
+      optional($._expression),
+      '}',
+    ),
+
+    boolean: $ => choice(
+      'True',
+      'False',
+    ),
+
+    none: $ => 'None',
+
+    float: $ => /\d*\.\d+/,
+
+    integer: $ => /\d+/,
+
+    string: $ => choice(
+      /".*"/,
+      /'.*'/,
+    ),
+
+    unary_expression: $ => choice(
+      prec.left(1, seq($.keyword, $._expression))
+    ),
+
+    binary_expression: $ => choice(
+      prec.left(2, seq($._expression, $.operator, $._expression))
+    ),
+
+    identifier: $ => /[a-z_]+/,
+
+
+    keyword: $ => choice(
+      'if',
+      'elif',
+      'else',
+      'endif',
+      'for',
+      'endfor',
+      'while',
+      'endwhile',
+      'macro',
+      'endmacro',
+      'set',
+    ),
+
+    operator: $ => choice(...operators),
+
+    _text: $ => prec.left(1, repeat1(/./))
+
   },
 });
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,4 +1,39 @@
-(comment) @comment
 
-(expression) @string
-(statement) @string
+; Variables
+(identifier) @variable
+
+; Functions
+(fn_declaration) @punctuation
+(fn_declaration
+  name: (identifier) @function
+  )
+
+; Literals
+[
+ (none)
+ (boolean)
+] @constant.builtin
+
+[
+ (float)
+ (integer)
+] @number
+
+(jinja_comment) @comment
+(string) @string
+
+(wrappers) @punctuation
+
+(jinja_expression) @tag
+(jinja_statement) @tag
+
+(operator) @operator
+
+[
+  (keyword)
+  "and"
+  "or"
+  "in"
+  "is"
+  "not"
+] @keyword
